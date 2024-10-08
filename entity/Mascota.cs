e30de869f513af58a771178aae0d6094755a8539
Cosmo_Black <147673998+Cosmo-Black-Dev@users.noreply.github.com>
2024-10-07 22:54:35 -0600

    Encapsule campos de clase Mascota

--- entity/Mascota.cs
+++ entity/Mascota.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,75 @@ namespace Clave1_Grupo2.entity
 {
     class Mascota
     {
+        private int idMascota;
+        private string nomMascota;
+        private int especie;
+        private string raza;
+        private int edad;
+        private char generoMascota;
+        private int idPropietario;
+        //public ArrayList<Cita> citas;
+
+
+        public int GetIdMascota() { return idMascota; }
+        public void SetIdmascota(int idMascota)
+        {
+            this.idMascota = idMascota;
+        }
+
+        public string GetNomMascota()
+        {
+            return nomMascota;
+        }
+        public void SetNomMascota(string nomMascota)
+        {
+            this.nomMascota= nomMascota;
+        }
+
+        public int GetEspecie()
+        {
+            return especie;
+        }
+        public void SetEspecie(int especie)
+        {
+            this.especie = especie;
+        }
+
+        public string GetRaza()
+        {
+            return raza;
+        }
+        public void SetRaza(string raza)
+        {
+            this.raza = raza;
+        }
+
+        public int GetEdad()
+        { 
+            return edad; 
+        }
+        public void SetEdad(int edad)
+        { 
+            this.edad = edad; 
+        }
+
+        public char GetGeneroMascota()
+        { 
+            return generoMascota; 
+        }
+        public void SetGeneroMascota(char generoMascota)
+        {
+            this.generoMascota = generoMascota;
+        }
+
+        public int GetIdPropietario()
+        {
+            return idPropietario;
+        }
+        public void SetIdPropietario(int idPropietario)
+        {
+            this.idPropietario = idPropietario;
+        }
+
     }
 }
