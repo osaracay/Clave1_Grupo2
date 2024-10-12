f3f4abcbd42d2f18d94c4292f3a69313b5890ca6
MiltonAgDev <aguirrep2016@gmail.com>
2024-10-12 17:30:48 -0600

    Form Compras terminado, Form Ventas Listo en Proceso de Codigo

--- gui/VtnVenta.cs
+++ gui/VtnVenta.cs
@@ -16,5 +16,26 @@ namespace Clave1_Grupo2.gui
         {
             InitializeComponent();
         }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+        private void LimpiarCampos()
+        {
+            cmbIdCliente.SelectedIndex = -1;
+            txtCliente.Text = "";
+            cmbIdInsumo.SelectedIndex = -1;
+            txtInsumo.Text = "";
+            txtMedida.Text = "";
+            cmbEstado.SelectedIndex = -1;
+            txtStock.Text = "";
+            txtPrecio.Text = "";
+            txtCantidad.Text = "";
+            cmbMetPago.SelectedIndex = -1;
+
+            dgvLstProductos.Rows.Clear();
+            txtTotal.Text = "";
+        }
     }
 }
