86653838a4db53bd97c934accd60327448943f9d
osaracay <ocsaracay@gmail.com>
2024-10-29 23:51:43 -0600

    detalles esteticos en agendar cita

--- gui/VtnAgendarCita.Designer.cs
+++ gui/VtnAgendarCita.Designer.cs
@@ -49,6 +49,7 @@ namespace Clave1_Grupo2.gui
             this.label3 = new System.Windows.Forms.Label();
             this.btnAgendar = new System.Windows.Forms.Button();
             this.btnGetCupos = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbxFotoPerfil)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
@@ -58,24 +59,23 @@ namespace Clave1_Grupo2.gui
             // 
             this.panel2.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.panel2.Controls.Add(this.pbxFotoPerfil);
-            this.panel2.Controls.Add(this.lblTitulo);
             this.panel2.Controls.Add(this.lblVetCatDog);
+            this.panel2.Controls.Add(this.lblTitulo);
             this.panel2.Controls.Add(this.picLogo);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(224, 450);
+            this.panel2.Size = new System.Drawing.Size(168, 373);
             this.panel2.TabIndex = 62;
             // 
             // pbxFotoPerfil
             // 
             this.pbxFotoPerfil.BackColor = System.Drawing.Color.Transparent;
             this.pbxFotoPerfil.Image = ((System.Drawing.Image)(resources.GetObject("pbxFotoPerfil.Image")));
-            this.pbxFotoPerfil.Location = new System.Drawing.Point(45, 15);
-            this.pbxFotoPerfil.Margin = new System.Windows.Forms.Padding(4);
+            this.pbxFotoPerfil.Location = new System.Drawing.Point(34, 12);
             this.pbxFotoPerfil.Name = "pbxFotoPerfil";
-            this.pbxFotoPerfil.Size = new System.Drawing.Size(148, 151);
+            this.pbxFotoPerfil.Size = new System.Drawing.Size(111, 123);
             this.pbxFotoPerfil.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxFotoPerfil.TabIndex = 63;
             this.pbxFotoPerfil.TabStop = false;
@@ -85,8 +85,7 @@ namespace Clave1_Grupo2.gui
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitulo.ForeColor = System.Drawing.Color.White;
-            this.lblTitulo.Location = new System.Drawing.Point(41, 170);
-            this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitulo.Location = new System.Drawing.Point(30, 138);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(119, 24);
             this.lblTitulo.TabIndex = 62;
@@ -97,7 +96,8 @@ namespace Clave1_Grupo2.gui
             this.lblVetCatDog.AutoSize = true;
             this.lblVetCatDog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblVetCatDog.ForeColor = System.Drawing.Color.White;
-            this.lblVetCatDog.Location = new System.Drawing.Point(96, 390);
+            this.lblVetCatDog.Location = new System.Drawing.Point(72, 317);
+            this.lblVetCatDog.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblVetCatDog.Name = "lblVetCatDog";
             this.lblVetCatDog.Size = new System.Drawing.Size(97, 40);
             this.lblVetCatDog.TabIndex = 6;
@@ -107,32 +107,35 @@ namespace Clave1_Grupo2.gui
             // 
             this.picLogo.Image = ((System.Drawing.Image)(resources.GetObject("picLogo.Image")));
             this.picLogo.InitialImage = ((System.Drawing.Image)(resources.GetObject("picLogo.InitialImage")));
-            this.picLogo.Location = new System.Drawing.Point(-3, 349);
-            this.picLogo.Margin = new System.Windows.Forms.Padding(4);
+            this.picLogo.Location = new System.Drawing.Point(-2, 284);
             this.picLogo.Name = "picLogo";
-            this.picLogo.Size = new System.Drawing.Size(99, 94);
+            this.picLogo.Size = new System.Drawing.Size(74, 76);
             this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picLogo.TabIndex = 0;
             this.picLogo.TabStop = false;
             // 
             // campoFechaAgenda
             // 
+            this.campoFechaAgenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.campoFechaAgenda.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.campoFechaAgenda.Location = new System.Drawing.Point(657, 27);
+            this.campoFechaAgenda.Location = new System.Drawing.Point(508, 7);
+            this.campoFechaAgenda.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.campoFechaAgenda.MaxDate = new System.DateTime(2025, 1, 18, 0, 0, 0, 0);
             this.campoFechaAgenda.MinDate = new System.DateTime(2024, 10, 18, 0, 0, 0, 0);
             this.campoFechaAgenda.Name = "campoFechaAgenda";
-            this.campoFechaAgenda.Size = new System.Drawing.Size(131, 22);
+            this.campoFechaAgenda.Size = new System.Drawing.Size(117, 22);
             this.campoFechaAgenda.TabIndex = 63;
             this.campoFechaAgenda.ValueChanged += new System.EventHandler(this.campoFechaAgenda_ValueChanged);
             // 
             // lbxCupos
             // 
+            this.lbxCupos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbxCupos.FormattingEnabled = true;
             this.lbxCupos.ItemHeight = 16;
-            this.lbxCupos.Location = new System.Drawing.Point(657, 55);
+            this.lbxCupos.Location = new System.Drawing.Point(508, 36);
+            this.lbxCupos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lbxCupos.Name = "lbxCupos";
-            this.lbxCupos.Size = new System.Drawing.Size(131, 308);
+            this.lbxCupos.Size = new System.Drawing.Size(117, 260);
             this.lbxCupos.TabIndex = 64;
             // 
             // cbxPropietario
@@ -142,9 +145,10 @@ namespace Clave1_Grupo2.gui
             this.cbxPropietario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxPropietario.ForeColor = System.Drawing.SystemColors.InactiveCaption;
             this.cbxPropietario.FormattingEnabled = true;
-            this.cbxPropietario.Location = new System.Drawing.Point(342, 87);
+            this.cbxPropietario.Location = new System.Drawing.Point(259, 88);
+            this.cbxPropietario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbxPropietario.Name = "cbxPropietario";
-            this.cbxPropietario.Size = new System.Drawing.Size(308, 23);
+            this.cbxPropietario.Size = new System.Drawing.Size(232, 23);
             this.cbxPropietario.TabIndex = 3;
             this.cbxPropietario.SelectedIndexChanged += new System.EventHandler(this.cbxPropietario_SelectedIndexChanged);
             // 
@@ -153,7 +157,8 @@ namespace Clave1_Grupo2.gui
             this.lblPropietario.AutoSize = true;
             this.lblPropietario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPropietario.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lblPropietario.Location = new System.Drawing.Point(229, 87);
+            this.lblPropietario.Location = new System.Drawing.Point(175, 88);
+            this.lblPropietario.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPropietario.Name = "lblPropietario";
             this.lblPropietario.Size = new System.Drawing.Size(77, 17);
             this.lblPropietario.TabIndex = 92;
@@ -166,9 +171,10 @@ namespace Clave1_Grupo2.gui
             this.cbxMascota.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxMascota.ForeColor = System.Drawing.SystemColors.InactiveCaption;
             this.cbxMascota.FormattingEnabled = true;
-            this.cbxMascota.Location = new System.Drawing.Point(342, 119);
+            this.cbxMascota.Location = new System.Drawing.Point(259, 114);
+            this.cbxMascota.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbxMascota.Name = "cbxMascota";
-            this.cbxMascota.Size = new System.Drawing.Size(308, 23);
+            this.cbxMascota.Size = new System.Drawing.Size(232, 23);
             this.cbxMascota.TabIndex = 4;
             // 
             // lblMascota
@@ -176,7 +182,8 @@ namespace Clave1_Grupo2.gui
             this.lblMascota.AutoSize = true;
             this.lblMascota.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblMascota.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lblMascota.Location = new System.Drawing.Point(229, 119);
+            this.lblMascota.Location = new System.Drawing.Point(175, 114);
+            this.lblMascota.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMascota.Name = "lblMascota";
             this.lblMascota.Size = new System.Drawing.Size(61, 17);
             this.lblMascota.TabIndex = 94;
@@ -189,9 +196,10 @@ namespace Clave1_Grupo2.gui
             this.cbxVeterinario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxVeterinario.ForeColor = System.Drawing.SystemColors.InactiveCaption;
             this.cbxVeterinario.FormattingEnabled = true;
-            this.cbxVeterinario.Location = new System.Drawing.Point(342, 23);
+            this.cbxVeterinario.Location = new System.Drawing.Point(259, 36);
+            this.cbxVeterinario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbxVeterinario.Name = "cbxVeterinario";
-            this.cbxVeterinario.Size = new System.Drawing.Size(308, 23);
+            this.cbxVeterinario.Size = new System.Drawing.Size(232, 23);
             this.cbxVeterinario.TabIndex = 1;
             this.cbxVeterinario.SelectedIndexChanged += new System.EventHandler(this.cbxVeterinario_SelectedIndexChanged);
             // 
@@ -200,7 +208,8 @@ namespace Clave1_Grupo2.gui
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(229, 23);
+            this.label1.Location = new System.Drawing.Point(175, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 17);
             this.label1.TabIndex = 96;
@@ -213,9 +222,10 @@ namespace Clave1_Grupo2.gui
             this.cbxTipoCita.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxTipoCita.ForeColor = System.Drawing.SystemColors.InactiveCaption;
             this.cbxTipoCita.FormattingEnabled = true;
-            this.cbxTipoCita.Location = new System.Drawing.Point(342, 55);
+            this.cbxTipoCita.Location = new System.Drawing.Point(259, 62);
+            this.cbxTipoCita.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbxTipoCita.Name = "cbxTipoCita";
-            this.cbxTipoCita.Size = new System.Drawing.Size(308, 23);
+            this.cbxTipoCita.Size = new System.Drawing.Size(232, 23);
             this.cbxTipoCita.TabIndex = 2;
             this.cbxTipoCita.SelectedIndexChanged += new System.EventHandler(this.cbxTipoCita_SelectedIndexChanged);
             this.cbxTipoCita.SelectedValueChanged += new System.EventHandler(this.cbxTipoCita_SelectedValueChanged);
@@ -225,7 +235,8 @@ namespace Clave1_Grupo2.gui
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(229, 55);
+            this.label2.Location = new System.Drawing.Point(175, 62);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(82, 17);
             this.label2.TabIndex = 98;
@@ -234,10 +245,11 @@ namespace Clave1_Grupo2.gui
             // txtMotivo
             // 
             this.txtMotivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMotivo.Location = new System.Drawing.Point(342, 182);
+            this.txtMotivo.Location = new System.Drawing.Point(259, 200);
+            this.txtMotivo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtMotivo.MaxLength = 250;
             this.txtMotivo.Name = "txtMotivo";
-            this.txtMotivo.Size = new System.Drawing.Size(308, 181);
+            this.txtMotivo.Size = new System.Drawing.Size(232, 160);
             this.txtMotivo.TabIndex = 99;
             this.txtMotivo.Text = "";
             // 
@@ -246,39 +258,62 @@ namespace Clave1_Grupo2.gui
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Location = new System.Drawing.Point(229, 155);
+            this.label3.Location = new System.Drawing.Point(179, 203);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(128, 17);
+            this.label3.Size = new System.Drawing.Size(73, 34);
             this.label3.TabIndex = 100;
-            this.label3.Text = "Motivo de su visita:";
+            this.label3.Text = "Motivo de \r\nsu visita:";
             // 
             // btnAgendar
             // 
-            this.btnAgendar.Location = new System.Drawing.Point(657, 375);
+            this.btnAgendar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAgendar.BackgroundImage")));
+            this.btnAgendar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAgendar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgendar.Location = new System.Drawing.Point(526, 300);
+            this.btnAgendar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAgendar.Name = "btnAgendar";
-            this.btnAgendar.Size = new System.Drawing.Size(131, 44);
+            this.btnAgendar.Size = new System.Drawing.Size(77, 64);
             this.btnAgendar.TabIndex = 101;
             this.btnAgendar.Text = "AGENDAR";
+            this.btnAgendar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnAgendar.UseVisualStyleBackColor = true;
             this.btnAgendar.Click += new System.EventHandler(this.btnAgendar_Click);
             // 
             // btnGetCupos
             // 
-            this.btnGetCupos.Location = new System.Drawing.Point(484, 149);
+            this.btnGetCupos.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnGetCupos.BackgroundImage")));
+            this.btnGetCupos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnGetCupos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetCupos.Location = new System.Drawing.Point(420, 142);
+            this.btnGetCupos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnGetCupos.Name = "btnGetCupos";
-            this.btnGetCupos.Size = new System.Drawing.Size(166, 23);
+            this.btnGetCupos.Size = new System.Drawing.Size(71, 49);
             this.btnGetCupos.TabIndex = 102;
             this.btnGetCupos.Text = "Consultar Cupos";
+            this.btnGetCupos.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.btnGetCupos.UseVisualStyleBackColor = true;
             this.btnGetCupos.Click += new System.EventHandler(this.btnGetCupos_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(173, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 24);
+            this.label4.TabIndex = 103;
+            this.label4.Text = "Agendar Cita";
+            // 
             // VtnAgendarCita
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(634, 373);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.btnGetCupos);
             this.Controls.Add(this.btnAgendar);
             this.Controls.Add(this.label3);
@@ -296,7 +331,7 @@ namespace Clave1_Grupo2.gui
             this.Controls.Add(this.panel2);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "VtnAgendarCita";
@@ -334,5 +369,6 @@ namespace Clave1_Grupo2.gui
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnAgendar;
         private System.Windows.Forms.Button btnGetCupos;
+        private System.Windows.Forms.Label label4;
     }
 }
